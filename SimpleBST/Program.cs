ead96882774dd05a2c393d361800537d55f7f2af
Lion Saedi <lionsaedi28@outlook.com>
2021-06-23 18:21:24 -0700

    I finished pre order and in order

--- SimpleBST/Program.cs
+++ SimpleBST/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// loop while we have something in our stack
+// loop all the way left
+// push left nodes along the way
+// collect value
+// pop
+// collect
+// go right once
+// start over (inner loop)
 class BSTNode
 {
     public int Value { get; set; }
     public BSTNode Left { get; set; }
     public BSTNode Right { get; set; }
 
-  
+
 }
 
 class Program
 {
- 
+
     static void Add(BSTNode root, int value)
     {
         BSTNode temp = root;
@@ -26,7 +34,7 @@ class Program
                     BSTNode holder = new BSTNode();
                     holder.Value = value;
                     temp.Left = holder;
-                    
+
                     break;
                 }
                 temp = temp.Left;
@@ -50,43 +58,68 @@ class Program
     // TODO
     static List<int> InOrderTraversal(BSTNode root, List<int> result)
     {
-        
         Stack<BSTNode> stack = new Stack<BSTNode>();
-
         BSTNode holder = root;
-
-        // loop while we have something in our stack
-           // loop all the way left
-           // push left nodes along the way
-           // collect value
-           // pop
-           // collect
-           // go right once
-           // start over (inner loop)
-
-        while (holder != null || result.Count > 0)
+        do
         {
-            //have multiple loops inside 
             if (holder != null)
             {
                 stack.Push(holder);
                 holder = holder.Left;
             }
-
             else
             {
                 holder = stack.Pop();
                 result.Add(holder.Value);
                 holder = holder.Right;
             }
-        }
-
-        
-
+        } while (stack.Count != 0);
 
         return result;
     }
 
+
+    static List<int> PreOrderTraversal(BSTNode root, List<int> result)
+    {
+        Stack<BSTNode> stack = new Stack<BSTNode>();
+        BSTNode holder = root;
+
+        stack.Push(root);
+
+        while (stack.Count != 0)
+        {
+            holder = stack.Pop();
+
+            result.Add(holder.Value);
+
+            if (holder.Right != null)
+            {
+                stack.Push(holder.Right);
+            }
+
+            if (holder.Left != null)
+            {
+                stack.Push(holder.Left);
+            }
+
+
+
+        }
+
+        return result;
+    }
+
+    static void PreOrderTraversalRecursive(BSTNode node, List<int> result)
+    {
+        if (node == null) return;
+
+        result.Add(node.Value);
+
+        PreOrderTraversalRecursive(node.Left, result);
+
+        PreOrderTraversalRecursive(node.Right, result);
+
+    }
 
     static void traverseRecursive(BSTNode node, List<int> result)
     {
@@ -103,19 +136,22 @@ class Program
     static void Main()
     {
         BSTNode root = new BSTNode() { Value = 42 };
-
+        //Add(root, root.Value);
+        Console.WriteLine(root.Value);
         Random random = new Random();
-        for (int i = 0; i < 25; i++)
+        for (int i = 0; i < 10; i++)
         {
             int value = random.Next(1, 100);
-          Console.WriteLine(value);
-          Add(root, value);
-           // Add(root, i);
+            Console.WriteLine(value);
+            Add(root, value);
+            // Add(root, i);
         }
 
-      List<int> result = new List<int>();
-        InOrderTraversal(root, result);
 
+
+        List<int> result = new List<int>();
+        //InOrderTraversal(root, result);
+        PreOrderTraversal(root, result);
         Console.WriteLine("");
 
         Console.WriteLine("");
@@ -124,8 +160,8 @@ class Program
         {
             Console.WriteLine(result[i]);
         }
-     
-        //    traverseRecursive(root, result);
+
+
         ;
     }
 }
